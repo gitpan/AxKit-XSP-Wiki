e4eb750fbf688c649be72b8e4807df2285d75c3f
Matt Sergeant <msergeant@cpan.org>
2003-12-15 11:27:11 +0000

    Import of MSERGEANT/AxKit-XSP-Wiki-0.07 from CPAN.
    gitpan-cpan-distribution: AxKit-XSP-Wiki
    gitpan-cpan-version:      0.07
    gitpan-cpan-path:         MSERGEANT/AxKit-XSP-Wiki-0.07.tar.gz
    gitpan-cpan-author:       MSERGEANT
    gitpan-cpan-maturity:     released

--- lib/AxKit/XSP/Wiki.pm
+++ lib/AxKit/XSP/Wiki.pm
@@ -2,10 +2,14 @@ package AxKit::XSP::Wiki;
 
 use strict;
 
-use Apache::AxKit::Language::XSP::TaglibHelper;
-use vars qw($VERSION $NS @ISA @EXPORT_TAGLIB);
+#use Apache::AxKit::Language::XSP::TaglibHelper;
+use AxKit::XSP::Wiki::Indexer;
+use Net::SMTP;
+use Text::Diff;
+use POSIX qw(strftime);
+use vars qw($VERSION $NS @ISA @EXPORT_TAGLIB $EmailAlerts $EmailHost);
 
-$VERSION = '0.06';
+$VERSION = '0.07';
 
 # The namespace associated with this taglib.
 $NS = 'http://axkit.org/NS/xsp/wiki/1';
@@ -15,6 +19,7 @@ $NS = 'http://axkit.org/NS/xsp/wiki/1';
 @EXPORT_TAGLIB = (
     'display_page($dbpath,$db,$page,$action;$id):as_xml=1',
     'preview_page($dbpath,$db,$page,$text,$texttype):as_xml=1',
+    'search($dbpath,$db,$query):as_xml=1',
 );
 
 use DBI;
@@ -31,10 +36,10 @@ sub _mkdb {
     );
     
     eval {
-	$db->do('select * from Page, Formatter, History where 1 = 2');
+        $db->do('select * from Page, Formatter, History where 1 = 2');
     };
     if ($@) {
-	create_db($db);
+        create_db($db);
     }
     
     return $db;
@@ -46,16 +51,16 @@ sub display_page ($$$$$) {
     my $db = _mkdb($dbpath, $dbname);
     
     if ($action eq 'edit') {
-	return edit_page($db, $page);
+        return edit_page($db, $page);
     }
     elsif ($action eq 'history') {
-	return show_history($db, $page);
+        return show_history($db, $page);
     }
     elsif ($action eq 'historypage') {
-	return show_history_page($db, $page, $id);
+        return show_history_page($db, $page, $id);
     }
     if ($action eq 'view') {
-	return view_page($db, $page);
+        return view_page($db, $page);
     }
     else {
         warn("Unrecognised action. Falling back to 'view'");
@@ -138,28 +143,28 @@ EOT
     my $output = '';
     my $handler = XML::SAX::Writer->new(Output => \$output);
     while ( my $row = $sth->fetch ) {
-	# create the parser
-	my $parser = $row->[1]->new(Handler => $handler);
-	eval {
-	    $parser->parse_string($row->[0]);
-	};
-	if ($@) {
-	    $output = '<pod>
+        # create the parser
+        my $parser = $row->[1]->new(Handler => $handler);
+        eval {
+            $parser->parse_string($row->[0]);
+        };
+        if ($@) {
+            $output = '<pod>
   <para>
     Error parsing the page: ' . xml_escape($@) . '
   </para>
 </pod>
   ';
-	}
-	last;
+        }
+        last;
     }
     if (!$output) {
-	$output = <<'EOT';
+        $output = <<'EOT';
 <newpage/>
 EOT
     }
     $output =~ s/^<\?xml\s.*?\?>//s;
-    AxKit::Debug(10, "Wiki Got: $output");
+    AxKit::Debug(10, "Wiki Got: $output") if $ENV{MOD_PERL};
     return $output;
 }
 
@@ -169,6 +174,16 @@ sub xml_escape {
     $text =~ s/</&lt;/g;
     $text =~ s/]]>/]]&gt;/g;
     return $text;
+}
+
+sub get_default_formatter {
+    my ($db) = @_;
+    my $sth = $db->prepare("SELECT id FROM Formatter WHERE name LIKE ?");
+    $sth->execute("pod%");
+    while (my $row = $sth->fetch) {
+        return $row->[0];
+    }
+    die "No rows from Formatter table!";
 }
 
 sub edit_page {
@@ -181,12 +196,12 @@ EOT
     $sth->execute($page);
     
     my $output = '<edit><text>';
-    my $formatter = 1;
+    my $formatter = get_default_formatter($db);
     while ( my $row = $sth->fetch ) {
-	# create the parser
-	$output .= xml_escape($row->[0]);
-	$formatter = $row->[1];
-	last;
+        # create the parser
+        $output .= xml_escape($row->[0]);
+        $formatter = $row->[1];
+        last;
     }
     $sth->finish;
     
@@ -198,9 +213,9 @@ EOT
 EOT
     $sth->execute();
     while (my $row = $sth->fetch) {
-	$output .= '<texttype id="'. xml_escape($row->[0]) . 
-	  ($formatter == $row->[0] ? '" selected="selected">' : '">') . 
-	  xml_escape($row->[1]) . '</texttype>';
+        $output .= '<texttype id="'. xml_escape($row->[0]) . 
+          ($formatter == $row->[0] ? '" selected="selected">' : '">') . 
+          xml_escape($row->[1]) . '</texttype>';
     }
     $sth->finish;
     
@@ -208,84 +223,213 @@ EOT
     return $output;
 }
 
-sub save_page {
-    my ($dbpath, $dbname, $page, $contents, $texttype, $ip, $rss) = @_;
-    $rss = [$rss, _mkrssheader($dbname)];
+sub search {
+    my ($dbpath, $dbname, $query) = @_;
     my $db = _mkdb($dbpath, $dbname);
-    _save_page($db, $page, $contents, $texttype, $ip, $rss);
+    my %search = parse_search($query);
+    use Data::Dumper; warn(Dumper(\%search));
+    my $results = search_message_index( db => $db,
+                                        required => $search{required},
+                                        normal => $search{normal},
+                                        phrase => $search{phrase},
+                                        excluded => $search{excluded},
+                                      );
+    my $output = '<search-results>';
+    if (!@{$results}) {
+        $output .= '<no-results/>';
+    }
+    foreach my $row (sort { $b->[1] <=> $a->[1] } @{$results}) {
+        $output .= "<result><page>" . xml_escape($row->[0]) . "</page>";
+        $output .= "<rank>" . xml_escape($row->[1]) . "</rank></result>";
+    }
+    $output .= "</search-results>";
+    warn("Search results: $output\n");
+    return $output;
+}
+
+sub search_message_index {
+    my %p = @_;
+    
+    my $db = $p{db};
+    
+    # Excluded words are excluded from all pages
+    my $exclude = '';
+    if ( @{$p{excluded}} ) {
+        $exclude .= "  AND Page.name NOT IN (
+            SELECT DISTINCT Page.name
+            FROM Page, ContentIndex, Word
+            WHERE ContentIndex.page_id = Page.id
+              AND ContentIndex.word_id = Word.id
+              AND Word.word IN (" .
+                    join(',', map { $db->quote($_) } @{$p{excluded}}) .
+                    ")
+                )\n";
+    }
+
+    my $sql = "
+SELECT Page.name, SUM(ContentIndex.value) AS value
+FROM ContentIndex, Page, Word
+WHERE ContentIndex.page_id = Page.id
+  AND ContentIndex.word_id = Word.id
+  AND (" . 
+        join(" OR ", (
+            (map { "Word.word = " . $db->quote($_) } @{$p{required}}),
+            (map { "Page.content LIKE " . $db->quote("\%$_\%") } @{$p{phrase}}),
+        )) .
+        ")
+$exclude
+GROUP BY ContentIndex.page_id
+";
+    warn("Getting required with:\n$sql\n");
+    return $db->selectall_arrayref($sql);
+}
+
+
+
+sub parse_search {
+    my $query = shift;
+    my %search;
+    while (defined $query && $query =~ /\G(\S*)(\s*)/gc) {
+        my $term = $1;
+        my $space = $2;
+        next unless length($term);
+
+        $term = lc($term);
+        
+        if ($term =~ s/^\+//) {
+            $search{required}{$term}++;
+            warn "Search required: $term\n";
+        }
+        elsif ($term =~ s/^\-//) {
+            $search{excluded}{$term}++;
+            warn "Search excluded: $term\n";
+        }
+        elsif ($term =~ /^(["'])/) {
+            my $quote = $1;
+            $term =~ s/^$quote//;
+            $term .= $space;
+
+            if ($query =~ /\G(.*?)\.?$quote\s*/gc) {
+                $term .= $1;
+                $search{phrase}{$term}++;
+                warn "Search phrase: $term\n";
+            }
+        }
+        else {
+            $search{required}{$term}++;
+            warn "Search normal: $term\n";
+        }
+    }
+
+    # turn into arrayrefs
+    foreach ( qw( normal required excluded phrase ) )
+    {
+        if ( $search{$_} )
+        {
+            $search{$_} = [ keys %{ $search{$_} } ]
+        }
+        else
+        {
+            $search{$_} = [];
+        }
+    }
+
+    return %search;
+}
+
+sub save_page {
+    my ($dbpath, $dbname, $page, $contents, $texttype, $ip, $user) = @_;
+    my $db = _mkdb($dbpath, $dbname);
+    _save_page($db, $page, $contents, $texttype, $ip, $user);
 }
 
 sub _save_page {
-    my ($db, $page, $contents, $texttype, $ip, $rss) = @_;
+    my ($db, $page, $contents, $texttype, $ip, $user) = @_;
     # NB fix hard coded formatterid
     my $last_modified = time;
+    my @history = $db->selectrow_array('SELECT content FROM History WHERE name = ? ORDER BY modified DESC', {}, $page);
     local $db->{AutoCommit} = 0;
-    my (@row) = $db->selectrow_array("SELECT * FROM Page WHERE name = ?", {}, $page);
-    if (@row) {
-        # store history
-        shift @row; # Remove id
-        $db->do('INSERT INTO History (name, formatterid, content, modified, ip_address)
-                 VALUES (?, ?, ?, ?, ?)', {}, @row);
-    }
-    else {
-        # New page
-        if ($rss->[0]) {
-            use Fatal qw(open close);
-            open(RSS, ">$rss->[0]");
-            flock(RSS, 2); # lock ex
-            print RSS $rss->[1];
-            my $sth = $db->prepare('SELECT * FROM Page ORDER BY last_modified DESC');
-            $sth->execute;
-            while (my $row = $sth->fetch) {
-                print RSS <<"EOT";
-<item>
-<title>$row->[1]</title>
-</item>
+    $db->do(<<'EOT', {}, $page, $texttype, $contents, $last_modified, $ip, $user);
+  INSERT OR REPLACE INTO Page ( name, formatterid, content, last_modified, ip_address, username )
+  VALUES ( ?, ?, ?, ?, ?, ? )
 EOT
-            }
-            print RSS "</rdf:RDF>\n";
-            flock(RSS, 8); # unlock
-            close(RSS);
-        }
-    }
-    my $sth = $db->prepare(<<'EOT');
-  INSERT OR REPLACE INTO Page ( name, formatterid, content, last_modified, ip_address )
-  VALUES ( ?, ?, ?, ?, ? )
+    $db->do(<<'EOT', {}, $page, $texttype, $contents, $last_modified, $ip, $user);
+  INSERT INTO History ( name, formatterid, content, modified, ip_address, username )
+  VALUES ( ?, ?, ?, ?, ?, ? )
 EOT
-    $sth->execute($page, $texttype, $contents, $last_modified, $ip);
     $db->commit;
+    _index_page($db, $page);
+    if ($EmailAlerts) {
+        # create diff using Text::Diff
+        my $prev = @history ? $history[0] : '';
+        my $diff = diff(\$prev, \$contents, { STYLE => 'Unified' });
+        
+        my $host = $EmailHost || 'localhost';
+        my $smtp = Net::SMTP->new($host, Timeout => 10);
+        $smtp->mail('axkitwiki') || die "Wiki email alerts: MAIL FROM:<axkitwiki> failed";
+        $smtp->to($EmailAlerts) || die "Wiki email alerts: RCPT TO:<$EmailAlerts> failed";
+        $smtp->data() || die "Wiki email alerts: DATA failed";
+        my $date = strftime('%a, %d %b %Y %H:%M:%S %Z', localtime);
+        
+        my $changed_by = $user ? "$user @ $ip" : "someone at IP $ip";
+        $smtp->datasend(<<"EOT");
+To: $EmailAlerts
+From: "AxKit Wiki" <axkitwiki>
+Subject: New Wiki Content at $page
+Date: $date
+
+Wiki content at $page Changed by $changed_by :
+
+$diff
+
+EOT
+        $smtp->dataend();
+        $smtp->quit();
+    }
 }
 
-sub _mkrssheader {
-    my ($dbname) = @_;
-    return <<"EOT";
-<rdf:RDF
-  xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-  xmlns:dc="http://purl.org/dc/elements/1.1/"
-  xmlns="http://purl.org/rss/1.0/"
->
-
- <channel rdf:about="http://take23.org/view/$dbname">
-   <title>$dbname</title>
-   <link>http://take23.org/view/$dbname</link>
-   <description>
-     Take23 $dbname
-   </description>
-
+sub _index_page {
+    my ($db, $page) = @_;
+    my $sth = $db->prepare(<<'EOT');
+  SELECT Page.id, Page.content, Formatter.module
+  FROM Page, Formatter
+  WHERE Page.formatterid = Formatter.id
+  AND   Page.name = ?
 EOT
+    $sth->execute($page);
+    
+    my $output = '';
+    while ( my $row = $sth->fetch ) {
+        my $handler = AxKit::XSP::Wiki::Indexer->new(DB => $db, PageId => $row->[0]);
+        # create the parser
+        my $parser = $row->[2]->new(Handler => $handler);
+        eval {
+            $parser->parse_string($row->[1]);
+        };
+        if ($@) {
+            warn("Indexing failed");
+        }
+        last;
+    }
 }
 
 sub show_history {
     my ($db, $page) = @_;
-    my $sth = $db->prepare('SELECT * FROM History WHERE name = ? ORDER BY modified DESC');
+    my $sth = $page ? $db->prepare('SELECT * FROM History WHERE name = ? ORDER BY modified DESC LIMIT 50') :
+                      $db->prepare('SELECT * FROM History ORDER BY modified DESC LIMIT 50');
     $sth->execute($page);
     my $hist = '<history>';
+    my %h;
+    my $cols = $sth->{NAME_lc};
     while (my $row = $sth->fetch) {
+        @h{@$cols} = @$row;
         $hist .= '<entry>';
-        $hist .= '<id>' . xml_escape($row->[0]) . '</id>';
-        $hist .= '<modified>' . xml_escape(scalar gmtime($row->[4])) . '</modified>';
-        $hist .= '<ip-address>' . xml_escape($row->[5]) . '</ip-address>';
-        $hist .= '<bytes>' . xml_escape(length($row->[3])) . '</bytes>';
+        $hist .= '<page>' . xml_escape($h{name}) . '</page>';
+        $hist .= '<id>' . xml_escape($h{id}) . '</id>';
+        $hist .= '<modified>' . xml_escape(scalar gmtime($h{modified})) . '</modified>';
+        $hist .= '<ip-address>' . xml_escape($h{ip_address}) . '</ip-address>';
+        $hist .= '<username>' . xml_escape($h{username}) . '</username>';
+        $hist .= '<bytes>' . xml_escape(length($h{content})) . '</bytes>';
         $hist .= '</entry>';
     }
     $hist .= '</history>';
@@ -309,23 +453,23 @@ EOT
     my ($ip, $modified);
     while ( my $row = $sth->fetch ) {
         ($ip, $modified) = ($row->[2], scalar(gmtime($row->[3])));
-	# create the parser
-	my $parser = $row->[1]->new(Handler => $handler);
-	eval {
-	    $parser->parse_string($row->[0]);
-	};
-	if ($@) {
-	    $output = '<pod>
+        # create the parser
+        my $parser = $row->[1]->new(Handler => $handler);
+        eval {
+            $parser->parse_string($row->[0]);
+        };
+        if ($@) {
+            $output = '<pod>
   <para>
     Error parsing the page: ' . xml_escape($@) . '
   </para>
 </pod>
   ';
-	}
-	last;
+        }
+        last;
     }
     if (!$output) {
-	$output = <<'EOT';
+        $output = <<'EOT';
 <pod>
   <para>
 Unable to find that history page, or unable to find formatter module
@@ -341,7 +485,7 @@ EOT
 }
 
 sub restore_page {
-    my ($dbpath, $dbname, $page, $ip, $id) = @_;
+    my ($dbpath, $dbname, $page, $ip, $id, $user) = @_;
     
     my $db = _mkdb($dbpath, $dbname);
     my $sth = $db->prepare('SELECT * FROM History WHERE name = ? and id = ?');
@@ -350,22 +494,26 @@ sub restore_page {
     die "No such row" unless $row;
     $sth->finish;
     my ($texttype, $contents) = ($row->[2], $row->[3]);
-    _save_page($db, $page, $contents, $texttype, $ip);
+    _save_page($db, $page, $contents, $texttype, $ip, $user);
 }
 
 sub create_db {
     my ($db) = @_;
     
     $db->do(q{
-	create table Page ( 
-			   id INTEGER PRIMARY KEY,
-			   name NOT NULL,
-			   formatterid NOT NULL,
-			   content,
-			   last_modified,
-                           ip_address
-			   )
+        create table Page ( 
+                           id INTEGER PRIMARY KEY,
+                           name NOT NULL,
+                           formatterid NOT NULL,
+                           content,
+                           last_modified,
+                           ip_address,
+                           username
+                           )
     });
+    $db->do(q{
+        create unique index Page_name on Page ( name )
+             });
     $db->do(q{
         create table History (
                               id INTEGER PRIMARY KEY, 
@@ -373,24 +521,49 @@ sub create_db {
                               formatterid NOT NULL,
                               content,
                               modified,
-                              ip_address
+                              ip_address,
+                              username
                              )
     });
     $db->do(q{
-	create unique index Page_name on Page ( name )
-	     });
+        CREATE TABLE IgnoreWord 
+        (
+         id INTEGER PRIMARY KEY,
+         word NOT NULL
+        )
+    });
+    $db->do(q{CREATE UNIQUE INDEX IgnoreWord_word on IgnoreWord (word)});
     $db->do(q{
-	create table Formatter ( id INTEGER PRIMARY KEY, module NOT NULL, name NOT NULL)
-	     });
+        CREATE TABLE Word 
+        (
+         id INTEGER PRIMARY KEY,
+         word NOT NULL
+        )
+    });
+    $db->do(q{CREATE UNIQUE INDEX Word_word on Word (word)});
     $db->do(q{
-	insert into Formatter (module, name) values ('Pod::SAX', 'pod - plain old documentation')
-	     });
+        CREATE TABLE ContentIndex
+        (
+         page_id     INTEGER             NOT NULL,
+         word_id     INTEGER             NOT NULL,
+         value       INTEGER             NOT NULL
+        )
+    });
     $db->do(q{
-	insert into Formatter (module, name) values ('Text::WikiFormat::SAX', 'wiki text')
-	     });
+        create unique index ContentIndex_idx on ContentIndex (page_id, word_id)
+    });
     $db->do(q{
-	insert into Formatter (module, name) values ('XML::LibXML::SAX::Parser', 'xml (freeform)')
-	     });
+        create table Formatter ( id INTEGER PRIMARY KEY, module NOT NULL, name NOT NULL)
+             });
+    $db->do(q{
+        insert into Formatter (module, name) values ('Pod::SAX', 'pod - plain old documentation')
+             });
+    $db->do(q{
+        insert into Formatter (module, name) values ('Text::WikiFormat::SAX', 'wiki text')
+             });
+    $db->do(q{
+        insert into Formatter (module, name) values ('XML::LibXML::SAX::Parser', 'xml (freeform)')
+             });
     $db->commit;
 }
 
--- webstuff/wiki.xsl
+++ webstuff/wiki.xsl
@@ -4,35 +4,50 @@
    	       xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
 >
 
+<xsl:include href="wikitags.xsl"/>
 <xsl:include href="pod.xsl"/>
 <xsl:include href="wikitext.xsl"/>
 <xsl:include href="docbook.xsl"/>
 <xsl:include href="sidemenu.xsl"/>
 
-<xsl:output method="html"/>
+<xsl:output method="html" doctype-public="-//W3C//DTD HTML 4.01 Transitional//EN"/>
 
 <xsl:param name="action" select="'view'"/>
+<xsl:param name="request.uri"/>
 
 <xsl:template match="/">
+  <xsl:variable name="side-menu-uri" select="concat('axkit:/wiki/view/', string(/xspwiki/db), '/SideMenu')"/>
     <html>
       <head>
-        <title><xsl:value-of select="/xspwiki/page"/></title>
-	<link rel="Stylesheet" href="/stylesheets/wiki.css"
+        <title>AxKit Wiki - <xsl:value-of select="/xspwiki/page"/></title>
+	<link rel="stylesheet" href="/wiki/wiki.css"
               type="text/css" media="screen" />
       </head>
 	
       <body>
-       <div class="topbanner">
-         Development Wiki
+       <div class="header">
+        <table width="100%" cellpadding="0" cellspacing="0" border="0"><tr>
+         <td width="50%"><img src="/img/axon-logo.png" alt="logo"/></td>
+         <td width="50%" align="right">
+          <div class="searchbanner">
+           <form action="./{/xspwiki/page}" method="GET">
+            <input type="hidden" name="action" value="search"/>
+            <input type="text" name="q" maxlength="255" size="20"/>
+            <input type="submit" value=" Search "/>
+           </form>
+          </div>
+         </td> 
+        </tr></table>
        </div>
-       <div class="base">
+        
+       <div class="main-content">
         <table><tr><td valign="top" width="160">
         <div class="sidemenu">
-         <xsl:apply-templates select="document('/sidemenu.xml')" mode="sidemenu"/>
+         <xsl:apply-templates select="document('/wiki/sidemenu.xml')" mode="sidemenu"/>
         </div></td><td valign="top" width="80%">
         <div class="maincontent">
          <div class="breadcrumbs">
-             <a href="DefaultPage"><xsl:value-of select="/xspwiki/db"/></a> :: <xsl:value-of select="/xspwiki/page"/>
+          <a href="/"><xsl:value-of select="/xspwiki/db"/></a> :: <a href="DefaultPage">Wiki</a> :: <xsl:value-of select="/xspwiki/page"/>
          </div>
          <hr/>
          <div class="content">
@@ -45,33 +60,45 @@
            </xsl:when>
           </xsl:choose>
        
-         <xsl:apply-templates/>
+         <xsl:apply-templates select="/xspwiki/main-content"/>
         
 	<xsl:choose>
 	  <xsl:when test="$action='view'">
-              <hr/>
-	    <a href="./{/xspwiki/page}?action=edit">Edit This Page</a> / <a href="./{/xspwiki/page}?action=history">Show Page History</a>
+	    <hr/>
+	    <a href="./{/xspwiki/page}?action=edit">Edit This Page</a>
+            / <a href="./{/xspwiki/page}?action=history">Show Page History</a>
 	  </xsl:when>
 	  <xsl:when test="$action='edit'">
-              <hr/>
+	    <hr/>
   	    <p><a href="EditTips">EditTips</a></p>
 	  </xsl:when>
           <xsl:when test="$action='historypage'">
-              <hr/>
-          <form action="./{/xspwiki/page}" method="POST">
+	    <hr/>
+            <form action="{substring-before($request.uri, '/view/')}/edit/{substring-after($request.uri, '/view/')}" method="POST" enctype="application/x-www-form-urlencoded">
            <input type="hidden" name="action" value="restore"/>
            <input type="hidden" name="id" value="{$id}"/>
            <input type="submit" name="Submit" value="Restore This Version"/>
           </form>
           </xsl:when>
           <xsl:when test="$action='history'">
+	    <hr/>
+          </xsl:when>
+          <xsl:when test="$action='search'">
               <hr/>
+              <div class="search">
+              <form action="./{/xspwiki/page}" method="GET">
+                  <input type="hidden" name="action" value="search"/>
+                  <input type="text" name="q" maxlength="255" size="20" value="{$q}"/>
+                  <input type="submit" value=" Search "/>
+              </form>
+              </div>
           </xsl:when>
 	  <xsl:otherwise>
 	  Other Mode?
 	  </xsl:otherwise>
 	</xsl:choose>
-
+          <hr/>
+          
          </div> <!-- content -->	
         </div> <!-- maincontent -->
         </td></tr></table>
@@ -80,84 +107,5 @@
 	
     </html>
 </xsl:template>
-
-<xsl:template match="/xspwiki/title"/>
-<xsl:template match="/xspwiki/page"/>
-<xsl:template match="/xspwiki/db"/>
-
-<xsl:template match="xspwiki">
-  <xsl:apply-templates/>
-</xsl:template>
-
-<xsl:template match="edit">
-<form action="./{/xspwiki/page}" method="POST" enctype="application/x-www-form-urlencoded">
-  <input type="hidden" name="action" value="save"/>
-  <h1><xsl:value-of select="/xspwiki/page"/> : 
-      <input type="submit" value=" Save "/>
-      <input type="submit" name="preview" value=" Preview "/></h1>
-  <textarea name="text" style="width:100%" rows="18" cols="80" wrap="virtual">
-    <xsl:value-of select="string(./text)"/>
-  </textarea>
-  <xsl:apply-templates select="./texttypes"/>
-</form>
-</xsl:template>
-
-<xsl:template match="texttypes">
-  Text Type: 
-  <select name="texttype">
-    <xsl:apply-templates/>
-  </select>
-</xsl:template>
-
-<xsl:template match="texttype">
-  <option value="{@id}">
-  <xsl:if test="@selected">
-  	  <xsl:attribute name="selected">selected</xsl:attribute>
-  </xsl:if>
-  	  <xsl:apply-templates/>
-  </option>
-</xsl:template>
-
-<xsl:template match="history">
-  <h1>History for <xsl:value-of select="/xspwiki/page"/></h1>
-  <table>
-  <tr><th>Date</th><th>IP Address</th><th>Bytes</th></tr>
-  <xsl:apply-templates select="./entry"/>
-  </table>
-</xsl:template>
-
-<xsl:template match="history/entry">
-  <tr>
-    <xsl:apply-templates/>
-  </tr>
-</xsl:template>
-
-<xsl:template match="history/entry/id">
-</xsl:template>
-
-<xsl:template match="history/entry/modified">
-  <td><a href="./{/xspwiki/page}?action=historypage;id={../id}"><xsl:apply-templates/></a></td>
-</xsl:template>
-
-<xsl:template match="history/entry/ip-address">
-  <td><xsl:apply-templates/></td>
-</xsl:template>
-
-<xsl:template match="history/entry/bytes">
-  <td><xsl:apply-templates/></td>
-</xsl:template>
-
-<xsl:template match="newpage">
-  <i>This page has not yet been created</i>
-</xsl:template>
-
-<!-- useful for testing - commented out for live
-<xsl:template match="node()|@*">
-  <xsl:copy>
-   <xsl:apply-templates select="@*"/>
-   <xsl:apply-templates/>
-  </xsl:copy>
-</xsl:template>
--->
 
 </xsl:stylesheet>
